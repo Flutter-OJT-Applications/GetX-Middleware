f19e3345946c2ab58e543edac2c1949bec89c21c
KyawZayarLynn <110543943+KyawZayarLynn@users.noreply.github.com>
2023-07-04 12:33:38 +0630

    Delete lib directory

--- lib/services/configs/initial_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/instance_manager.dart';
-
-
-class InitialBinding extends Bindings{
-  @override
-  void dependencies() {
-    // TODO: Define the initial bindings here
-  }
-}
--- lib/services/home/home_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeService extends GetxController{
-  @override
-  void onInit() {
-    print(">>> HomeService called");
-    super.onInit();
-  }
-}
