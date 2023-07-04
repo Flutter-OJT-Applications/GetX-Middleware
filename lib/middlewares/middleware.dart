27bbe19d925ca675d772b2a6c8da149596ceda7b
scm-sai-zaw-myint <scm.saizawmyint@gmail.com>
2023-07-04 09:18:04 +0630

    initial commit

---
+++ lib/middlewares/middleware.dart
@@ -0,0 +1,35 @@
+import 'package:authentications/services/login/login_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import '../services/auth/auth_service.dart';
+
+class GlobalMiddleware extends GetMiddleware {
+  final authController = Get.find<AuthService>();
+
+  @override
+  RouteSettings? redirect(String? route) {
+    return authController.authenticated || route == '/login'
+        ? null
+        : const RouteSettings(name: '/login');
+  }
+  //
+  // @override
+  // GetPage? onPageCalled(GetPage? page) {
+  //   print('>>> Page ${page!.name} called');
+  //   print('>>> User ${authController.username} logged');
+  //   return page;
+  // }
+  //
+  // @override
+  // GetPageBuilder? onPageBuildStart(GetPageBuilder? page) {
+  //   print('Bindings of ${page.toString()} are ready');
+  //   return page;
+  // }
+  //
+  // @override
+  // Widget onPageBuilt(Widget page) {
+  //   print('Widget ${page.toStringShort()} will be showed');
+  //   return page;
+  // }
+
+}
